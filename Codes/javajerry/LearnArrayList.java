63709b6474352f4de9df55aa417cf97671683f8f
Shuhan Cao <139927650+Mcillum102@users.noreply.github.com>
2024-11-24 23:27:30 +0000

    Added ArrayList class
    Co-authored-by: FlyingPig1230 <FlyingPig1230@users.noreply.github.com>

---
+++ Codes/javajerry/LearnArrayList.java
@@ -0,0 +1,22 @@
+package javajerry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LearnArrayList {
+
+    public static void main(String[] args) {
+        // Create an ArrayList
+        List<String> cars = new ArrayList<String>();
+
+        List<Integer> carYear = new ArrayList<Integer>();
+
+
+        // Add elements
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+    }
+    
+}
