29438e65f058b4f4eae142393d496e48002467d8
Shuhan Cao <139927650+Mcillum102@users.noreply.github.com>
2024-12-08 20:14:43 +0000

    Adding 2nd arraylist
    Co-authored-by: FlyingPig1230 <FlyingPig1230@users.noreply.github.com>

---
+++ Codes/javajerry/ArrayListLesson2.java
@@ -0,0 +1,30 @@
+package javajerry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListLesson2 {
+
+    public static void main(String[] args) {
+        // Create an ArrayList
+        List<String> cars = new ArrayList<String>();
+
+        List<Integer> carYear = new ArrayList<Integer>();
+
+        cars.add("Tesla");
+        cars.add("BMW");
+        cars.add("Audi");
+        cars.add("Ford");
+        cars.add(2, "Benz");
+        carYear.add(123);
+        System.out.println(cars.size());
+        cars.set(3, "Toyota");
+        cars.remove(cars.size() - 1);
+
+        System.out.println(cars);
+        /* How to print each car's brand name out */
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
+        }
+    }
+}
