655a184923a0fbf99fccd4eee18238faa2b50f5a
Mcillum102 <139927650+Mcillum102@users.noreply.github.com>
2025-01-11 19:51:10 -0800

    Added code for Q10

--- Codes/APCSA_2025/Finals/TestReview.java
+++ Codes/APCSA_2025/Finals/TestReview.java
@@ -1,6 +1,22 @@
 public class TestReview {
 
+    public static void changer(String x, int y) {
+        x = x + "peace";
+        y = y * 2;
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public static void test() {
+        String s = "world";
+        int n = 6;
+        changer(s, n);
+        /* End of method */
+        System.out.println(s);
+        System.out.println(n);
+    }
+
     public static void main(String[] args) {
-        
+        test();
     }
 }
