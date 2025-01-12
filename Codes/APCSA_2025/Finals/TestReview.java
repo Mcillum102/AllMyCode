fd2761961dc278635ee41c4ad99690d0e3e5cd89
Mcillum102 <139927650+Mcillum102@users.noreply.github.com>
2025-01-11 19:57:24 -0800

    Q11: Difference between Global and Local variable

--- Codes/APCSA_2025/Finals/TestReview.java
+++ Codes/APCSA_2025/Finals/TestReview.java
@@ -1,10 +1,13 @@
 public class TestReview {
 
+    private static String word;
+    
     public static void changer(String x, int y) {
         x = x + "peace";
         y = y * 2;
         System.out.println(x);
         System.out.println(y);
+        word = "2";
     }
 
     public static void test() {
@@ -14,6 +17,7 @@ public class TestReview {
         /* End of method */
         System.out.println(s);
         System.out.println(n);
+        word = " 8";
     }
 
     public static void main(String[] args) {
