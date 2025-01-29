4712f65f63cdc5af55188a457e9442cccaad412b
Shuhan Cao <139927650+Mcillum102@users.noreply.github.com>
2025-01-29 00:15:07 +0000

    Learned about ACSLC3

--- Codes/Eric_Python/ACSL/ACSLC3_P.py
+++ Codes/Eric_Python/ACSL/ACSLC3_P.py
@@ -1,12 +1,39 @@
 '''Solution'''
-fibo_list = [1, 2]
-for i in range(2, 7):
-    fibo_list.append(fibo_list[-2] + fibo_list[-1])
-print(fibo_list)
+# fibo_list = [1, 2]
+# for i in range(2, 7):
+#     fibo_list.append(fibo_list[-2] + fibo_list[-1])
+# # print(fibo_list)
+# # Based on the numbers in this list, how do I change h to the correct letter
+# # How to add all numbers in fibo_list to letter 'h'?
+# increased_amount = []
+# # ord('h') gives you the unicode for h. 
+# # You want to add h with each number in the fibo_list
+# for i in fibo_list:
+#     increased_amount.append((ord("h") + i - ord('a')) % 26 + ord('a'))
+# # print(increased_amount)
+# # Next step, we want to add all numbers in increased_amount to each of the letters in original string
+# org_str = "ACSL c2"
+# # How to add them as: A + 105, C + 106. For each letter, you are using their unicode
+# answer = ""
+# for i in range(len(org_str)):
+#     answer += str(ord(org_str[i]) + increased_amount[i]) + " "
+# print(answer[:-1])
 
-# Based on the numbers in this list, how do I change h to the correct letter
-# How to add all numbers in fibo_list to letter 'h'?
-answer = []
-answer.append(ord("h"))
-for i in range():
-    
+
+'''Practices'''
+# No 1:
+# String Question -> end_other
+# def end_other(a, b):
+#   if a.lower().endswith(b.lower()):
+#     return True
+#   if b.lower().endswith(a.lower()):
+#     return True
+#   else:
+#     return False
+# a = input()
+# b = input()
+# print(end_other(a,b))
+
+# No 2:
+string = "zopzop"
+stringlist = []
