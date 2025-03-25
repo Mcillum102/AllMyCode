7ec3a937aa3d957553e9203cde9cedefb3f82311
Shuhan Cao <139927650+Mcillum102@users.noreply.github.com>
2025-03-25 03:02:50 +0000

    ACSL Q4 2013-2014
    Co-authored-by: Eric <Epixx512@users.noreply.github.com>

--- Codes/Eric_Python/ACSL/ACSLQ4.py
+++ Codes/Eric_Python/ACSL/ACSLQ4.py
@@ -41,23 +41,84 @@
 # print(different_letters)
 
 # Try 3:
-in1 = "2 Computer BAT"
+# in1 = "7 the quick brown fox"
 # 1. how to extract both 2 and 'Computer BAT' from in1?
 # 1a. 2 is an int, Computer must be uppercase
-in1 = in1.upper()
-n = int(in1[0])
-word = in1[2:]
-word_list = list(word)
-sorted_list = [word_list[0]]
-for i in range(1, len(word_list)):
-    for j in range(len(sorted_list)):
-        curi = word_list[i]
-        curj = sorted_list[j]
-        if curi.isalpha() and curj.isalpha():
-            if ord(curi) <= ord(curj):
-                sorted_list.insert(j, curi)
-                break
-    else:
-        sorted_list.append(curi)
+# in1 = in1.upper()
+# n = int(in1[0]) - 1     # postion n is 1 more than its index
+# word = in1[2:]
+# word_list = list(word)
+# sorted_list = [word_list[0]]
+# print(word_list)
+# How to remove the spaces from word_list?
+# Remove the spaces until the list has no space
+# Hint: use if and loop to compare each item in word_list with ' '. 
+# If it == ' ', we remove it using .remove(?) ? is what you need to remove
+# Try it below:
+# for i in word_list:
+#     if i == ' ':
+#         word_list.remove(i)
+
+# How to find out the number of times index 2 contains number?
+# You need to add something in your current code to be able to calculate that
+
+# When a new letter appears at postion n in sorted_list, you want to record it down
+# into somewhere
+
+# Try to create a list to store the letters, 
+# or create a int variable as count to count the number of times
+# don't worry if your code is not perfect
+# uniques = []
+# for i in range(1, len(word_list)):
+#     for j in range(len(sorted_list)):
+#         curi = word_list[i]
+#         curj = sorted_list[j]
+#         if ord(curi) <= ord(curj):
+#             sorted_list.insert(j, curi)
+#             break
+#     else:
+#         sorted_list.append(curi)
+#     if n < len(sorted_list) and not sorted_list[n] in uniques :
+#         uniques.append(sorted_list[n])
         
-print(sorted_list)
+# print(uniques)
+# print(len(uniques))
+
+'''2013-2014'''
+in1 = "9678415, 4"
+in1 = in1.split(", ")
+
+numbers = list(map(int, in1[0]))
+length = int(in1[1])
+
+print(numbers)
+# how to write a loop that can add up everything in a list?
+total = 0
+largest = 0
+best = []
+# for i in str_number:
+#     total += int(i)
+#     # Find out if total is a multiple of 5 and if it is larger than largest
+#     if total % 5 == 0 and total > largest:
+#         largest = total
+for i in range(1, len(numbers)):
+    stop = length
+    sub1 = numbers[0 : i]
+    if len(sub1) == length:
+        break
+    for j in range(i, length+i):
+        sub2 = numbers[j : stop]
+        sub3 = sub1 + sub2
+        if len(sub3) != length:
+            break
+        total = sum(sub3)
+        if total % 5 == 0 and total > largest:
+            largest = total
+            best = sub3
+        stop += 1
+print(largest)
+# Which variable do I print? How to print each number from high to low?
+best.sort(reverse=True)
+for i in best:
+    print(i, end="")
+print()
