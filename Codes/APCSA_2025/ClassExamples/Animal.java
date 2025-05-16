fbcc90af4995006eca5dd5440251cf6ec2cd335d
Mcillum102 <139927650+Mcillum102@users.noreply.github.com>
2025-05-16 16:55:05 -0700

    ll

--- Codes/APCSA_2025/ClassExamples/Animal.java
+++ Codes/APCSA_2025/ClassExamples/Animal.java
@@ -1,5 +1,3 @@
-
-
 public class Animal {
     
     // Fields / Properties
---
+++ Codes/FRI_Python/practice.py
@@ -0,0 +1,30 @@
+# please type your code here 
+inpu = '''314159265
+11223344
+25897257
+91
+9876543210'''
+# When your input contains multiple lines, we must separate them
+inpu = inpu.split('\n')       # split by the \n change line symbol can make each line an item in a list
+
+
+for e in inpu:
+    inp = e         # intake a string
+    # check/run trough each digit? using loop?
+    larger = 0      # compare with larger number to pick the next correct number 
+    num = ''
+    outputs = []
+    for i in range(len(inp)):
+        # the above for loop uses indexes, not the items themselves
+        # you want to compare the current digit with larger
+        num += inp[i]
+        # adding each digits to num if the digit is not larger than larger
+        if int(num) > larger:
+            larger = int(num)
+            outputs.append(num)
+            num = ''
+            # in the condition here, we set larger to the new number and add it
+            # into the list. We also reset num.
+
+    # as outputs list contains your answer now, how to print each number out with a space separating them
+    print(' '.join(outputs))
