cd6601c7f6a012d33adb6b6c849235346df44a5a
Mcillum102 <139927650+Mcillum102@users.noreply.github.com>
2025-05-17 20:34:59 -0700

    111

--- Codes/FRI_Python/practice.py
+++ Codes/FRI_Python/practice.py
@@ -1,9 +1,12 @@
+'''
+Practice 1
+
 # please type your code here 
-inpu = '''314159265
+inpu = '314159265
 11223344
 25897257
 91
-9876543210'''
+9876543210'
 # When your input contains multiple lines, we must separate them
 inpu = inpu.split('\n')       # split by the \n change line symbol can make each line an item in a list
 
@@ -28,3 +31,24 @@ for e in inpu:
 
     # as outputs list contains your answer now, how to print each number out with a space separating them
     print(' '.join(outputs))
+    
+'''
+# Create a 8x8 matrix 
+matrix = [[0 for _ in range(8)] for _ in range(8)]
+
+inp = "2, 434, 523, 43"
+inp = inp.split(', ')
+R = int(inp.pop(0))      # number of rows that has tiles turned on
+print(R)
+# loop R times
+for i in range(R):
+    cord = list(inp[i])
+    r = int(cord.pop(0))
+    for c in cord:
+        matrix[r-1][int(c)-1] = 1
+press_pos = list(inp[-1])
+print(press_pos)
+
+
+print(matrix)
+
