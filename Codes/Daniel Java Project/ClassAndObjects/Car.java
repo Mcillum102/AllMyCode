6ad81ccbf197cf7e097cbf448dfc690957e099eb
Shuhan Cao <139927650+Mcillum102@users.noreply.github.com>
2025-07-04 20:19:16 +0000

    111

---
+++ Codes/Daniel Java Project/ClassAndObjects/Car.java
@@ -0,0 +1,33 @@
+public class Car {
+
+    // Properties / Fields
+    // These information are usually the identities of something
+    // We usually use private properties
+    private double speed;
+    private int yearBuilt;
+    private String color; 
+    
+    // Constructor
+    // The methods that creates Objects
+    // Contructor will always be public
+    // public Car() {}     // deafult constructor
+    public Car(double theSpeed, int theYearBuilt, String theColor) {
+        speed = theSpeed;
+        yearBuilt = theYearBuilt;
+        color = theColor;
+        // Assigning parameters into local properties
+        // parameter are variables that accepts values from other places that calls the method
+    }
+    
+
+
+
+
+    public static void main(String[] args) {
+
+        Car c = new Car(110.5, 2021, "white");      // This will create a car object that's stored in variable c
+        // The first Car is the type of the variable
+        // The second Car() is a constructor
+        System.out.println(c.speed);
+    }
+}
