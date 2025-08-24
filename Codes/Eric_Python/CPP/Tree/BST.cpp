7c61e5470b942ebcdf4d3a4e0b6850595300673c
Mcillum102 <139927650+Mcillum102@users.noreply.github.com>
2025-08-23 18:19:00 -0700

    Eric - C++ BST

--- Codes/Eric_Python/CPP/Tree/BST.cpp
+++ Codes/Eric_Python/CPP/Tree/BST.cpp
@@ -12,7 +12,7 @@ struct Person {
 
 // A tree is formed based on a special structure;
 struct Node {
-// identities
+// identities/attributes/fields/properties
     int value;
     Node* left;
     Node* right;
@@ -20,19 +20,83 @@ struct Node {
 
 // If we want to create struct contain value, a constructor must be here
 // The name of the consturctor must match the struct name
-// Node: for those that you know the type >> (parameters) : identities(parameters)
+// Node: for those that you know the type >> (type parameters) : identities(parameters)
 //       for those that you may not have type or pointes >> identities(nullptr)
 // nullptr means null, nothing contained in the variable
+    Node() : value(0), left(nullptr), right(nullptr) {}
     Node(int val) : value(val), left(nullptr), right(nullptr) {}
 };
 
-int main() {
-    vector<int> numbers = {13, 7, 15, 3, 8, 14, 19, 18};
-    vector<Node> tree;
-    
-    for (int i = 0; i < 8; i++) {
-        tree.push_back(Node(numbers.at(i)));
+Node* createTree(vector<int> numbers) {
+    // If the vector is empty, return nullptr. Prevent the program from crashing.
+    if (numbers.empty()) {
+        return nullptr;
     }
+
+    Node* root = new Node(numbers.at(0));
+    
+    for (int i = 1; i < numbers.size(); i++) {
+        Node* current = root;
+        Node* newNode = new Node(numbers.at(i));
+        
+        while (true) {
+            // For pointers, we use -> to access the value.
+            if (numbers.at(i) < current->value) {
+                if (current->left == nullptr) {
+                    current->left = newNode;
+                    break;
+                } else {
+                    current = current->left;
+                }
+            } else {
+                if (current->right == nullptr) {
+                    current->right = newNode;
+                    break;
+                } else {
+                    current = current->right;
+                }
+            }
+        }
+    }
+    
+    return root;
+}
+
+void printTree(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    
+    cout << "[";
+    
+    // Print left subtree
+    printTree(root->left);
+    
+    // Print current node
+    cout << root->value;
+    
+    // Print right subtree
+    printTree(root->right);
+    
+    cout << "]";
+}
+
+int main() {
+    vector<int> numbers;
+    numbers.push_back(13);
+    numbers.push_back(7);
+    numbers.push_back(15);
+    numbers.push_back(3);
+    numbers.push_back(8);
+    numbers.push_back(14);
+    numbers.push_back(19);
+    numbers.push_back(18);
+    cout << numbers.size() << endl;     // endl means new line ("\n")
+    Node* tree = createTree(numbers);
+    
+    cout << "BST as list: ";
+    printTree(tree);
+    cout << endl;
     
     // the left and right are defaultly set to null. no need to declare here
 
