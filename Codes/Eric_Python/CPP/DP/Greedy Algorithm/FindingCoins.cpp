ec3730e714b2e59d962fb9c28343c46e5744c5e0
Shuhan Cao <139927650+Mcillum102@users.noreply.github.com>
2025-09-23 00:09:09 +0000

    some changes

--- Codes/Eric_Python/CPP/DP/Greedy Algorithm/FindingCoins.cpp
+++ Codes/Eric_Python/CPP/DP/Greedy Algorithm/FindingCoins.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+using namespace std;
 
+// Very simple greedy algorithm example
+// Keep in mind that not all case would work in greedy algorithm.
+int main() {
     vector<int> coins = {1,2,5,10,20,50,100,200};
     int money = 120;
 
