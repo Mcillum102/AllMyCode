f20564d9f0466f65955c26316cb7c3f00bcc4b11
Shuhan Cao <139927650+Mcillum102@users.noreply.github.com>
2025-10-09 03:30:19 +0000

    Eric - Practice

--- Codes/Eric_Python/CPP/Divide&Conquer/NumberOfOcc.cpp
+++ Codes/Eric_Python/CPP/Divide&Conquer/NumberOfOcc.cpp
@@ -2,6 +2,22 @@
 #include <vector>
 using namespace std;
 
-int main() {
+int count(vector<int> &arr, int target) {
     
+}
+
+int main()
+{
+    vector<int> arr = {1, 1, 2, 2, 2, 2, 3};
+    // int target = 3;
+    // int ans = 0;
+    // for (int i : arr)
+    // {
+    //     if (i == target)
+    //         ans++;
+    // }
+    // cout << ans << endl;
+    
+
+    return 0;
 }
